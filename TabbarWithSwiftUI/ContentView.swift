e8ef99182337a6c1ed3ffcc8d244a102f8c219e4
ikawashima41 <i.kawashima41@gmail.com>
2020-08-13 19:46:01 +0900

    Initial Commit

---
+++ TabbarWithSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TabbarWithSwiftUI
+//
+//  Created by Iichiro Kawashima on 2020/08/13.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TabbarWithSwiftUI/FirstViewController.swift
@@ -0,0 +1,30 @@
+//
+//  FirstViewController.swift
+//  TabbarWithSwiftUI
+//
+//  Created by Iichiro Kawashima on 2020/08/13.
+//  Copyright © 2020 Iichiro Kawashima. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
